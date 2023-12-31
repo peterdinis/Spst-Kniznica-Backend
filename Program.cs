17b07bee75eaedbc7b2483d7a47c4dea00317fe6
Peterdinis <peterdinis0@pobox.sk>
2023-12-31 11:50:44 +0100

    FOO

--- Program.cs
+++ Program.cs
@@ -1,7 +1,53 @@
 using LibrarySPSTApi.data;
+using LibrarySPSTApi.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddIdentity<Student, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+
+// Config Identity
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 3;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase= false;
+    options.Password.RequireUppercase= false;
+    options.Password.RequireNonAlphanumeric= false;
+    options.SignIn.RequireConfirmedEmail = false;
+});
+
+
+// Add Authentication and JwtBearer
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.RequireHttpsMetadata= false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+        };
+    });
+
 
 // Add services to the container.
 
--- data/DataContext.cs
+++ data/DataContext.cs
@@ -8,5 +8,4 @@ public class DataContext: DbContext
     public DataContext(DbContextOptions<DataContext> options): base(options) {}
     
     public DbSet<Category> Categories { get; set; }
-    public DbSet<Student> Students { get; set; }
 }
