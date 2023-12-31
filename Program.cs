f5d1f237714599bba735107dafd4f59dab72a2e4
Peterdinis <peterdinis0@pobox.sk>
2023-12-31 11:15:16 +0100

    feat: Create first table

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using LibrarySPSTApi.data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
---
+++ data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LibrarySPSTApi.Entities;
+
+namespace LibrarySPSTApi.data;
+
+public class DataContext: DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options): base(options) {}
+    
+    public DbSet<Category> Categories { get; set; }
+}
