3157b3f16d46d955ad4b52e2c0edc54bcfad2142
Peterdinis <peterdinis0@pobox.sk>
2024-01-01 20:21:29 +0100

    feat: Create new table

---
+++ Entities/Book.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibrarySPSTApi.Entities;
+
+[Table("books")]
+public class Book
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string AuthorName { get; set; } = null!;
+    
+    public int Pages { get; set; }
+
+    public string Status { get; set; } = null!;
+    
+    public DateTime Year { get; set; }
+
+    public string Image { get; set; } = null!;
+
+    public int Quantity { get; set; }
+
+}
