6589e2d297299c848f9b4147d9082c906e8176bc
Peterdinis <peterdinis0@pobox.sk>
2024-01-02 18:47:28 +0100

    feat: Categories DONE

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,6 +1,68 @@
-﻿namespace LibrarySPSTApi.Controllers;
+﻿using LibrarySPSTApi.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using LibrarySPSTApi.Entities;
 
-public class CategoryController
+namespace LibrarySPSTApi.Controllers
 {
-    
+    [Route("/Categories")] 
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService ?? throw new ArgumentNullException(nameof(categoryService));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
+        {
+            var categories = await _categoryService.GetAllCategoriesAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategoryById(int id)
+        {
+            var category = await _categoryService.GetCategoryByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Category>> CreateCategory(Category category)
+        {
+            var createdCategory = await _categoryService.CreateCategoryAsync(category);
+            return CreatedAtAction(nameof(GetCategoryById), new { id = createdCategory.Id }, createdCategory);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Category>> UpdateCategory(int id, Category category)
+        {
+            var updatedCategory = await _categoryService.UpdateCategoryAsync(id, category);
+            if (updatedCategory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(updatedCategory);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCategory(int id)
+        {
+            var deleted = await _categoryService.DeleteCategoryAsync(id);
+            if (!deleted)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
 }
