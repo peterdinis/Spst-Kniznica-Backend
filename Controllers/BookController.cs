f1b96254aecc579d3a5867c2c12c97ef7914b1fb
Peterdinis <peterdinis0@pobox.sk>
2024-01-04 19:48:00 +0100

    feat: Start fixing warnings WIP

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -6,26 +6,19 @@ namespace LibrarySPSTApi.Controllers
 {
     [ApiController]
     [Route("/Books")] 
-    public class BookController : ControllerBase
+    public class BookController(IBookService bookService) : ControllerBase
     {
-        private readonly IBookService _bookService;
-
-        public BookController(IBookService bookService)
-        {
-            _bookService = bookService;
-        }
-
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Book>>> GetAllBooks()
         {
-            var books = await _bookService.GetAllBooksAsync();
+            var books = await bookService.GetAllBooksAsync();
             return Ok(books);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Book>> GetBookById(int id)
         {
-            var book = await _bookService.GetBookByIdAsync(id);
+            var book = await bookService.GetBookByIdAsync(id);
 
             if (book == null)
             {
@@ -38,19 +31,14 @@ namespace LibrarySPSTApi.Controllers
         [HttpPost]
         public async Task<ActionResult<Book>> AddBook(Book book)
         {
-            var addedBook = await _bookService.AddBookAsync(book);
-            return CreatedAtAction(nameof(GetBookById), new { id = addedBook.Id }, addedBook);
+            var addedBook = await bookService.AddBookAsync(book);
+            return CreatedAtAction(nameof(GetBookById), new { id = addedBook!.Id }, addedBook);
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateBook(int id, Book book)
         {
-            var updatedBook = await _bookService.UpdateBookAsync(id, book);
-
-            if (updatedBook == null)
-            {
-                return NotFound();
-            }
+            await bookService.UpdateBookAsync(id, book);
 
             return NoContent();
         }
@@ -58,7 +46,7 @@ namespace LibrarySPSTApi.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteBook(int id)
         {
-            var result = await _bookService.DeleteBookAsync(id);
+            var result = await bookService.DeleteBookAsync(id);
 
             if (!result)
             {
