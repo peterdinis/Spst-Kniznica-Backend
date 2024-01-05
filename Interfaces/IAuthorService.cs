b56964256f6411b4cde196b341a305c971ddd51a
Peterdinis <peterdinis0@pobox.sk>
2024-01-05 17:43:59 +0100

    feat: Start to fixing broken author service

---
+++ Interfaces/IAuthorService.cs
@@ -0,0 +1,12 @@
+﻿using LibrarySPSTApi.Entities;
+
+namespace LibrarySPSTApi.Interfaces
+{
+    public interface IAuthorService {
+        Task<IEnumerable<Author?>> GetAllAuthorsAsync();
+        Task<Author?> GetAuthorByIdAsync(int id);
+        Task<Author?> AddAuthorAsync(Author author);
+        Task<Author> UpdateAuthorAsync(int id, Author author);
+        Task<bool> DeleteAuthorAsync(int id);
+    }
+}
